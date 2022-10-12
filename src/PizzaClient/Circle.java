b8fe0582e06b95146f2ceefdb4b25a0ca3e1cb9b
KEOBOLAKOTAdeza <113565415+KEOBOLAKOTAdeza@users.noreply.github.com>
2022-10-13 00:54:28 +0700

    hhh

---
+++ src/PizzaClient/Circle.java
@@ -0,0 +1,14 @@
+package PizzaClient;
+
+class Circle implements IShape {
+    private double _radius;
+    public Circle(double r) {
+        _radius = r;
+    }
+
+
+
+    public double getArea() {
+        return Math.PI * _radius * _radius;
+    }
+}
